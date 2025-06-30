dc3b027f9ef7925fa3268d103c57d5a27d4e045b
서창열 <kongbaguni@gmail.com>
2025-06-30 13:02:50 +0900

    Initial Commit

---
+++ LightMeter/LightMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightMeterApp.swift
+//  LightMeter
+//
+//  Created by Changyeol Seo on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct LightMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LightMeterTests/LightMeterTests.swift
@@ -0,0 +1,17 @@
+//
+//  LightMeterTests.swift
+//  LightMeterTests
+//
+//  Created by Changyeol Seo on 6/30/25.
+//
+
+import Testing
+@testable import LightMeter
+
+struct LightMeterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

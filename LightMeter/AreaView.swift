8b766124a9b8e9d5b3f56581c086b9b0e1842a2d
서창열 <kongbaguni@gmail.com>
2025-07-18 17:38:18 +0900

    노출계 기능 구현

---
+++ LightMeter/AreaView.swift
@@ -0,0 +1,18 @@
+//
+//  AreaView.swift
+//  LightMeter
+//
+//  Created by Changyeol Seo on 7/18/25.
+//
+
+import SwiftUI
+
+struct AreaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AreaView()
+}
---
+++ LightMeter/ControllerView.swift
@@ -0,0 +1,18 @@
+//
+//  ControllerView.swift
+//  LightMeter
+//
+//  Created by Changyeol Seo on 7/18/25.
+//
+
+import SwiftUI
+
+struct ControllerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ControllerView()
+}

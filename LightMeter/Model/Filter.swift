8e05ed79eedce885f40333cacce82ca590f484d8
서창열 <kongbaguni@gmail.com>
2025-11-16 09:08:59 +0900

    1.3.2

--- LightMeter/Model/Filter.swift
+++ LightMeter/Model/Filter.swift
@@ -7,6 +7,7 @@
 import Foundation
 import SwiftUI
 
+
 enum FilterType : Int, CaseIterable {
     case clear
     case yellow
@@ -15,6 +16,17 @@ enum FilterType : Int, CaseIterable {
     case green
     case blue
     case polarizer
+    
+    case ND2
+    case ND4
+    case ND8
+    case ND16
+    case ND32
+    case ND64
+    case ND128
+    case ND256
+    case ND512
+    case ND1024
     
     var stop : Double {
         switch self {
@@ -32,6 +44,26 @@ enum FilterType : Int, CaseIterable {
             return 2
         case .polarizer:
             return 2
+        case .ND2:
+            return 1
+        case .ND4:
+            return 2
+        case .ND8:
+            return 3
+        case .ND16:
+            return 4
+        case .ND32:
+            return 5
+        case .ND64:
+            return 6
+        case .ND128:
+            return 7
+        case .ND256:
+            return 8
+        case .ND512:
+            return 9
+        case .ND1024:
+            return 10
         }
     }
     
@@ -51,25 +83,66 @@ enum FilterType : Int, CaseIterable {
             return .blue
         case .polarizer:
             return .black
+        case .ND2:
+            return .black.opacity(0.1)
+        case .ND4:
+            return .black.opacity(0.2)
+        case .ND8:
+            return .black.opacity(0.3)
+        case .ND16:
+            return .black.opacity(0.4)
+        case .ND32:
+            return .black.opacity(0.5)
+        case .ND64:
+            return .black.opacity(0.6)
+        case .ND128:
+            return .black.opacity(0.7)
+        case .ND256:
+            return .black.opacity(0.8)
+        case .ND512:
+            return .black.opacity(0.9)
+        case .ND1024:
+            return .black
         }
     }
     
     var label : Text {
         switch self {
         case .red:
-            return Text("Red")
+            return .init("Red")
         case .orange:
-            return Text("Orange")
+            return .init("Orange")
         case .yellow:
-            return Text("Yellow")
+            return .init("Yellow")
         case .clear:
-            return Text("Clear")
+            return .init("Clear")
         case .green:
-            return Text("Green")
+            return .init("Green")
         case .blue:
-            return Text("Blue")
+            return .init("Blue")
         case .polarizer:
-            return Text("Polarizer")
+            return .init("Polarizer")
+        case .ND2:
+            return .init("ND2")
+        case .ND4:
+            return .init("ND4")
+        case .ND8:
+            return .init("ND8")
+        case .ND16:
+            return .init("ND16")
+        case .ND32:
+            return .init("ND32")
+        case .ND64:
+            return .init("ND64")
+        case .ND128:
+            return .init("ND128")
+        case .ND256:
+            return .init("ND256")
+        case .ND512:
+            return .init("ND512")
+        case .ND1024:
+            return .init("ND1024")
+
         }
     }
 }
